1b1702fe2c3169e8bee7c364683d223ebcdf2102
Bernice Tran <BerniceTran@bernices-macbook-air.local>
2015-10-14 19:49:49 -0400

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Bernice Tran on 10/14/15.
+//  Copyright © 2015 Bernice Tran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
